a69069dbfa2e7e7b83124b6318b355c99fbc2072
Zohar Sabari <zohar1000@gmail.comm>
2020-06-24 16:30:36 +0300

    fixed logt() build warning message

--- src/utils/zconsole.ts
+++ src/utils/zconsole.ts
@@ -1,7 +1,5 @@
 import { ZTime } from './ztime';
 
 export const logt = (...args) => {
-  args.unshift(ZTime.localUniDateTimeMs() + ' ==>');
-  // @ts-ignore
-  console.log.apply(this, args);
+  console.log(ZTime.localUniDateTimeMs(), '==>', ...args);
 };
